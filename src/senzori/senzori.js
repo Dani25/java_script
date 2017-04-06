96baa219c1469f1a4debf99a5615073c8648beef
Daniela Onita <danielaonita25@gmail.com>
2017-04-06 19:47:34 +0300

    desenare cerc

--- src/senzori/senzori.js
+++ src/senzori/senzori.js
@@ -6,8 +6,16 @@ function on_device_orientation(evt)
 	var beta = evt.beta;
 	var gamma = evt.gamma;
 		
-	document.getElementById("a").innerHTML = "alpha = "+alpha.toString();
-	document.getElementById("b").innerHTML = "beta = "+beta.toString();
-	document.getElementById("c").innerHTML = "gamma = "+gamma.toString();
-
+	document.getElementById("a").innerHTML = "alpha = "+alpha;
+	document.getElementById("b").innerHTML = "beta = "+beta;
+	document.getElementById("c").innerHTML = "gamma = "+gamma;
+	
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext("2d");
+	context.clearRect(0,0,canvas.width, canvas.height);
+	var raza = 50; 
+	var centru = {x:canvas.width/2, y:canvas.height/2}
+	context.beginPath();
+	context.arc(centru.x+gamma*(canvas.width/2-raza)/90, centru.y+beta*(canvas.height/2-raza)/90, raza, 0, 2 * Math.PI);
+	context.stroke();
 }
