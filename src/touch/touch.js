a8ab18b1a59d61dc5e8a0497b5ae36fba92a1b2a
Daniela Onita <danielaonita25@gmail.com>
2017-04-07 19:54:45 +0300

    desenare crcuri colorate

--- src/touch/touch.js
+++ src/touch/touch.js
@@ -1,5 +1,5 @@
 window.addEventListener("touchstart", on_touch_start);
-
+var touch_colors = ["#FF0000", "#00FF00", "#0000FF", "0F0F00"];
 function on_touch_start(evt)
 {
 	var touches = evt.changedTouches;
@@ -9,6 +9,7 @@ function on_touch_start(evt)
 	for (var i = 0; i< touches.length; i++)
 	{
 		context.beginPath();
+		context.strokeStyle = touch_colors[i];
 		context.arc(touches[i].pageX, touches[i].pageY, 25, 0, 2* Math.PI);
 		context.stroke();
 	}
