d597520a1d2ca000301c889926dbe7de9002cbad
Daniela Onita <danielaonita25@gmail.com>
2017-05-04 19:14:27 +0300

    2 cercuri

--- src/SVG/svg.js
+++ src/SVG/svg.js
@@ -1,10 +1,15 @@
 var c1=document.getElementById("cerc1");
-
+var c2=document.getElementById("c2");
 c1.addEventListener("touchmove", on_touch_move);
+c2.addEventListener("touchmove", on_touch_move);
 
-function on_touch_move(e){
-	
-	var t = e.changedTouches[0];
-	c1.getAttribute("cx",t.pageX);
-	c1.getAttribute("cy",t.pageY);
+function on_touch_move(e)
+{
+	for (var i=0;i<e.changedTouches.length;i++)
+	{
+		var t = e.changedTouches[i];
+		var c = t.target;
+		c.setAttribute("cx",t.pageX);
+		c.setAttribute("cy",t.pageY);
 	}
+}
