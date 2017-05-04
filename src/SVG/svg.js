ad9b7c1541a3d2ccd52fc415d1cc75f2f69ea36b
Daniela Onita <danielaonita25@gmail.com>
2017-05-04 18:32:34 +0300

    svg

---
+++ src/SVG/svg.js
@@ -0,0 +1,10 @@
+var c1=document.getElementById("cerc1");
+
+c1.addEventListener("touchmove", on_touch_move);
+
+function on_touch_move(e){
+	
+	var t = e.changedTouches[0];
+	c1.getAttribute("cx",t.pageX);
+	c1.getAttribute("cy",t.pageY);
+	}
