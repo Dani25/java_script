7fc977be2e709c5e04c6b1bf68ffb0689ad88d88
Daniela Onita <danielaonita25@gmail.com>
2017-05-05 19:38:28 +0300

    speech recognition

---
+++ src/speech/speech.js
@@ -0,0 +1,21 @@
+document.addEventListener("touchstart",on_touch);
+document.addEventListener("mousedown",mouse_down);
+
+var recognition = new webkitSpeechRecognition();
+recognition.lang = "en-US";
+
+function on_touch()
+{
+	recognition.start();
+	
+}
+function onend()
+{
+	recognition.stop();
+}
+recognition.onend = onend();
+recognition.onsoundend = onend();
+recognition.onresult = function (e)
+{
+	document.getElementById("text").innerHTML = e.results[0][0].transcript;
+}
--- src/speech/synt.js
+++ src/speech/synt.js
@@ -14,6 +14,7 @@ function speak()
 {
 	var ut = new SpeechSynthesisUtterance();
 	ut.text = document.getElementById("text").value;
+	ut.rate =10;
 	synt.speak(ut);
 }
 //-----------------------------------------------------------------------------------
