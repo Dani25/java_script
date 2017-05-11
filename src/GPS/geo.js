ba355bf8bf10152125e08d4baba843f22f15869b
Daniela Onita <danielaonita25@gmail.com>
2017-05-11 19:42:36 +0300

    add maps

--- src/GPS/geo.js
+++ src/GPS/geo.js
@@ -1,7 +1,9 @@
 get_geo_position();
 function on_success(position){
-	document.getElementById("text").innerHTML = position.coords.latitude + " " + position.coords.longitude;
-}
+	document.getElementById("text").innerHTML = "latitude= "+position.coords.latitude + " longitude= " + position.coords.longitude+" acc= "+position.coords.accuracy+" speed="+position.coords.speed+ "altitude="+position.coords.altitude;
+	var map_str = "http://maps.googleapis.com/maps/api/staticmap?"+"center="+position.coords.latitude+", "+ position.coords.longitute+"&zoom=17&size=400x300&key=AIzaSyArIA_t1lsVeGMDTwmjBKX-UbvwRaGq0T0";
+	document.getElementById("map").setAttribute("src",map_str);
+	}
 function on_error(e)
 {
 	document.getElementById("text").innerHTML = e.message;
