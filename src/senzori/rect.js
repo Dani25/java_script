05902ef6efe295b4bcd4efbb351ea8ce839094fa
Daniela Onita <danielaonita25@gmail.com>
2020-10-14 11:27:09 +0300

    Update rect.js

--- src/senzori/rect.js
+++ src/senzori/rect.js
@@ -21,7 +21,7 @@ function on_device_orientation(evt)
 	
 	context.translate(centru.x/2, centru.y/2);
 	
-	context.rotate(alpha * Math.PI / 180);
+	context.rotate(beta * Math.PI / 180);
 	
 	context.beginPath();
 	context.rect(-latura_patrat/2, -latura_patrat/2, latura_patrat, latura_patrat)
