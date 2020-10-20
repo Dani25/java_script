99ea6cb008249e78f0f5889aa47ca16eb59a9134
Daniela Onita <danielaonita25@gmail.com>
2020-10-20 14:26:24 +0300

    Update geo.js

--- src/GPS/geo.js
+++ src/GPS/geo.js
@@ -1,11 +1,12 @@
 get_geo_position();
 function on_success(position){
-	document.getElementById("latitude").innerHTML = "latitude= "+position.coords.latitude;
+	document.getElementById("latitude").innerHTML = position.coords.latitude;
 	document.getElementById("longitude").innerHTML = " longitude= " + position.coords.longitude;
 	document.getElementById("acc").innerHTML = "acc="+position.coords.accuracy;
 	document.getElementById("altitude").innerHTML = "altitude = "+position.coords.altitude;
 	document.getElementById("text").innerHTML = "latitude= "+position.coords.latitude + " longitude= " + position.coords.longitude+" acc= "+position.coords.accuracy+" speed="+position.coords.speed+ "altitude="+position.coords.altitude;
-	
+	document.getElementById("text").innerHTML = "latitude= "+position.coords.latitude;
+
 	
 	//var map_str = "http://maps.googleapis.com/maps/api/staticmap?"+"center="+position.coords.latitude+", "+ position.coords.longitute+"&zoom=17&size=400x300&key=AIzaSyArIA_t1lsVeGMDTwmjBKX-UbvwRaGq0T0";
 	var map_str = "http://maps.google.com/maps/api/staticmap?sensor=true" + position.coords.latitude + "," +
