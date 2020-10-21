c95cd02c6dee33de0933d6e2269692b18823bccf
Daniela Onita <danielaonita25@gmail.com>
2020-10-21 16:52:52 +0300

    Update speech.js

--- src/speech/speech.js
+++ src/speech/speech.js
@@ -25,7 +25,7 @@ recognition.onresult =on_results;
 
 function on_results(e)
 {
-	var alternatives = e.results[2];
+	var alternatives = e.results[0];
 	for (var i=0; i<alternatives.length; i++)
 		document.getElementById("text").innerHTML += alternatives[i].transcript 
 	//document.getElementById("text").innerHTML += "Ati rostit cuvantul: "+ alternatives[i].transcript + ", acuratete: "+alternatives[i].confidence + "<br>" ;
